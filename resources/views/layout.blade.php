84e1f3ea44782eaab4f95f48d817d2fc700a268c
anditsung <anditsung@gmail.com>
2020-09-21 10:00:12 +0700

    crftoken on meta

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-{{--    <meta name="csrf-token" content="{{ csrf_token() }}">--}}
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ \Laravel\Nova\Nova::name() }}</title>
 
@@ -24,12 +24,12 @@
     </div>
 
     <!-- Scripts -->
-    <script type="application/javascript">
-        window.Laravel = {!! json_encode([
-                            'csrfToken' => csrf_token(),
-                            'apiToken' => Auth()->user()->api_token ?? null,
-                        ]) !!}
-    </script>
+{{--    <script type="application/javascript">--}}
+{{--        window.Laravel = {!! json_encode([--}}
+{{--                            'csrfToken' => csrf_token(),--}}
+{{--                            'apiToken' => Auth()->user()->api_token ?? null,--}}
+{{--                        ]) !!}--}}
+{{--    </script>--}}
     <script src="{{ mix('manifest.js', 'vendor/novaweb') }}"></script>
     <script src="{{ mix('vendor.js', 'vendor/novaweb') }}"></script>
     <script src="{{ mix('app.js', 'vendor/novaweb') }}"></script>
